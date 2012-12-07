2c00e612bd995027e06a5f77c9682ebe3a4570f8
BobHunsicker <rhunsicker@catalystitservices.com>
2012-12-07 16:00:51 -0500

    Updated pubspec.yaml.

---
+++ packages/log4dart/log4dart.dart
@@ -0,0 +1,21 @@
+// Copyright (c) 2012 Solvr, Inc. All rights reserved.
+//
+// This open source software is governed by the license terms 
+// specified in the LICENSE file
+
+#library("log4dart");
+
+#source("src/appenders/console_appender.dart");
+#source("src/appenders/string_appender.dart");
+
+#source("src/appender.dart");
+#source("src/logger.dart");
+#source("src/logger_factory.dart");
+#source("src/logger_impl.dart");
+#source("src/logger_config.dart");
+#source("src/logger_config_map.dart");
+#source("src/log_format_reader.dart");
+#source("src/log_level.dart");
+#source("src/log_record.dart");
+#source("src/log_record_formatter.dart");
+
